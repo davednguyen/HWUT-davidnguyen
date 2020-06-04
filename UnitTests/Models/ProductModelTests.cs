cb9425abf60b78253e45580f7554cbfacdebf631
dnguyen <david.nguyen@smartsheet.com>
2020-06-03 18:30:57 -0700

    update all existing test cases

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -37,8 +37,7 @@ namespace UnitTests
         public void ProductModel_Get_ID_Default_Should_Pass()
         {
             var result = new ProductModel();
-            result.Id = "12345";
-            Assert.AreEqual("12345", result.Id);
+            Assert.AreEqual("12345", result.Id = "12345");
         }
 
         [TestMethod]
@@ -52,9 +51,11 @@ namespace UnitTests
         [TestMethod]
         public void ProductModel_Get_Image_Default_Should_Pass()
         {
+            string expectImage = "http://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBWPuIz.img?ocid=msaut1";
+
             var result = new ProductModel();
             result.Image = "http://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBWPuIz.img?ocid=msaut1";
-            string expectImage = "http://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBWPuIz.img?ocid=msaut1";
+            
             Assert.AreEqual(expectImage, result.Image);
         }
 
@@ -62,24 +63,26 @@ namespace UnitTests
         public void ProductModel_Get_URL_Default_Should_Pass()
         {
             var result = new ProductModel();
-            result.Url = "https://www.tesla.com/";
-            Assert.AreEqual("https://www.tesla.com/", result.Url);
+
+            Assert.AreEqual("https://www.tesla.com/", result.Url = "https://www.tesla.com/");
         }
 
         [TestMethod]
         public void ProductModel_Get_Title_Default_Should_Pass()
         {
             var result = new ProductModel();
-            result.Title = "Model 3";
-            Assert.AreEqual("Model 3", result.Title);
+
+            Assert.AreEqual("Model 3", result.Title = "Model 3");
         }
 
         [TestMethod]
         public void ProductModel_Get_Description_Default_Should_Pass()
         {
+            string des = "322mi Range 145mph Top Speed 4.4s 0 - 60 mph";
+
             var result = new ProductModel();
             result.Description = "322mi Range 145mph Top Speed 4.4s 0 - 60 mph";
-            string des = "322mi Range 145mph Top Speed 4.4s 0 - 60 mph";
+
             Assert.AreEqual(des, result.Description);
         }
 
@@ -87,24 +90,24 @@ namespace UnitTests
         public void ProductModel_Get_Sequence_Default_Should_Pass()
         {
             var result = new ProductModel();
-            result.Sequence = "1";
-            Assert.AreEqual("1", result.Sequence);
+
+            Assert.AreEqual("1", result.Sequence = "1");
         }
 
         [TestMethod]
         public void ProductModel_Get_Email_Default_Should_Pass()
         {
             var result = new ProductModel();
-            result.Email = "test@test.info";
-            Assert.AreEqual("test@test.info", result.Email);
+
+            Assert.AreEqual("test@test.info", result.Email = "test@test.info");
         }
 
         [TestMethod]
         public void ProductModel_Get_Logistics_Default_Should_Pass()
         {
             var result = new ProductModel();
-            result.Logistics = "1";
-            Assert.AreEqual("1", result.Logistics);
+
+            Assert.AreEqual("1", result.Logistics = "1");
         }
 
         [TestMethod]
